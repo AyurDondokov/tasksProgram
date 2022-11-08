56ba95504418b1e7af9ad2a5ea2b12364730980d
AyurDondokov <ayur00dondokov@gmail.com>
2022-11-08 12:55:49 +0800

    6

--- tasksProgram.py
+++ tasksProgram.py
@@ -1,4 +1,6 @@
 import time
+
+from math import sqrt
 from memory_profiler import profile
 
 @profile
@@ -55,7 +57,42 @@ def z3_2():
     print("%5.1f secs" % (time_elapsed))
 
 def z4():
+    n = int(input("Введите число: "))
+    if sqrt(5 * (n ** 2) - 4) % 1 == 0 or sqrt(5 * (n ** 2) + 4) % 1 == 0:
+        print("Из ряда фибоначи")
+    else:
+        print("Не из ряда фибоначи")
 
-
-z3()
-z3_2()
+def z5_1():
+    n = int(input("Введите номер месяца: "))
+    if (n < 3 or n == 12):
+        print("Зима")
+    elif (2 < n <= 5):
+        print("Весна")
+    elif (5 < n <= 8):
+        print("Лето")
+    else:
+        print("Осень")
+def z5_2():
+    n = int(input("Введите номер месяца: "))
+    d = {
+        "Зима": (12, 1, 2),
+        "Весна": (3, 4, 5),
+        "Лето": (6, 7, 8),
+        "Осень": (9, 10, 11)
+    }
+    for key in d:
+        if (n in d[key]):
+            print(key)
+def z6():
+    x = int(input("Введите X: "))
+    s1, s2 = 0, 0
+    if (x % 2 == 0):
+        s1 = (1 + x-1) * (x // 2) / 2
+        s2 = (2 + x) * (x // 2) / 2
+    else:
+        s1 = (1 + x) * (x // 2) / 2
+        s2 = (2 + x-1) * (x // 2) / 2
+    print(f"Сумма всех нечетных: {s1}")
+    print(f"Сумма всех четных: {s2}")
+z6()
